59122e9e15fc6ebd5116f5f0537b223fd00af6c7
Hoger Dehnhardt <holger@dehnhardt.org>
2018-01-30 23:27:30 +0100

    register all modules in the repository

--- src/Flaarlib.cpp
+++ src/Flaarlib.cpp
@@ -9,57 +9,91 @@
 
 #include "Flaarlib.h"
 #include "FLModule.h"
-#include "modules/InputModule.h"
+#include "modules/FLInputModule.h"
 #include "exceptions/Exceptions.h"
 #include "helper/LoopDetector.h"
+#include "FLRepository.h"
+#include "modules/FLAudioFileInputModule.h"
+#include "modules/FLMIDIFileInputModule.h"
+#include "modules/FLAudioFileOutputModule.h"
+#include "modules/FLVolumeControl.h"
 
 #include <vector>
 #include <algorithm>
 
-namespace flaarlib {
 
-FLAARLIB_API FLLogger * FLLog::fllogger_ = 0;
+namespace flaarlib
+{
 
-Flaarlib::Flaarlib() {
-	FLLog::info("Init FLLib");
+FLAARLIB_API FLLogger *FLLog::fllogger_ = 0;
+
+Flaarlib::Flaarlib()
+{
+	FLLog::debug("Init FLLib");
 }
 
-Flaarlib::~Flaarlib() {
+Flaarlib::~Flaarlib()
+{
 }
 
 /**
  * Initialize the g3 logging library as a fast async logging facility
  */
-void Flaarlib::initializeLogging() {
+void Flaarlib::initializeLogging()
+{
 }
 
-void Flaarlib::addInputModule(InputModule* inputModule) {
+bool Flaarlib::createRepository()
+{
+	m_pRepository = new FLRepository();
+	m_pRepository->registerModule( new FLAudioFileInputModule("r_AudioFileInputModule", -1));
+	m_pRepository->registerModule( new FLMIDIFileInputModule("r_MIDIFileInputModule"));
+	m_pRepository->registerModule( new FLAudioFileOutputModule("r_AudioFileOutputModule", -1));
+	m_pRepository->registerModule( new FLVolumeControl("r_VolumeControl", -1, -1));
+	return true;
+}
+
+void Flaarlib::addInputModule(FLInputModule *inputModule)
+{
 	m_inputModules[inputModule->getModuleName()] = inputModule;
 }
 
-void Flaarlib::addModule(FLModule* module) {
+bool Flaarlib::init()
+{
+	bool ret = true;
+	ret &= createRepository();
+	return(ret);
+}
+
+void Flaarlib::addModule(FLModule *module)
+{
 	string moduleName = module->getModuleName();
 	if (!m_modules.push_back_unique(moduleName))
 		throw new ConfigurationExecption(
 				ConfigurationExceptionType::NAME_NOT_UNIQUE, "ABC", "DEF");
 }
 
-void Flaarlib::loopDetection() {
-	map<string, InputModule*>::iterator it;
-	try {
-		for (it = m_inputModules.begin(); it != m_inputModules.end(); ++it) {
+void Flaarlib::loopDetection()
+{
+	map<string, FLInputModule *>::iterator it;
+	try
+	{
+		for (it = m_inputModules.begin(); it != m_inputModules.end(); ++it)
+		{
 			vector<string> moduleNames;
 			FLModule *m = it->second;
 			UniqueVector<string> v;
 			new LoopDetector(m, v);
 		}
 		m_configurationValid = true;
-	} catch (ConfigurationExecption *e) {
+	}
+	catch (ConfigurationExecption *e)
+	{
 		FLLog::error(e->getMessage());
 	}
 }
 
-Flaarlib* Flaarlib::_instance = 0;
+Flaarlib *Flaarlib::_instance = 0;
 
 } /* namespace flaarlib */
 
--- src/Flaarlib.h
+++ src/Flaarlib.h
@@ -7,19 +7,19 @@
 
 // Generic helper definitions for shared library support
 #if defined _WIN32 || defined __CYGWIN__
-  #define FLAARLIB_HELPER_DLL_IMPORT __declspec(dllimport)
-  #define FLAARLIB_HELPER_DLL_EXPORT __declspec(dllexport)
-  #define FLAARLIB_HELPER_DLL_LOCAL
+#define FLAARLIB_HELPER_DLL_IMPORT __declspec(dllimport)
+#define FLAARLIB_HELPER_DLL_EXPORT __declspec(dllexport)
+#define FLAARLIB_HELPER_DLL_LOCAL
 #else
-  #if __GNUC__ >= 4
-    #define FLAARLIB_HELPER_DLL_IMPORT __attribute__ ((visibility ("default")))
-    #define FLAARLIB_HELPER_DLL_EXPORT __attribute__ ((visibility ("default")))
-    #define FLAARLIB_HELPER_DLL_LOCAL  __attribute__ ((visibility ("hidden")))
-  #else
-    #define FLAARLIB_HELPER_DLL_IMPORT
-    #define FLAARLIB_HELPER_DLL_EXPORT
-    #define FLAARLIB_HELPER_DLL_LOCAL
-  #endif
+#if __GNUC__ >= 4
+#define FLAARLIB_HELPER_DLL_IMPORT __attribute__ ((visibility ("default")))
+#define FLAARLIB_HELPER_DLL_EXPORT __attribute__ ((visibility ("default")))
+#define FLAARLIB_HELPER_DLL_LOCAL  __attribute__ ((visibility ("hidden")))
+#else
+#define FLAARLIB_HELPER_DLL_IMPORT
+#define FLAARLIB_HELPER_DLL_EXPORT
+#define FLAARLIB_HELPER_DLL_LOCAL
+#endif
 #endif
 
 // Now we use the generic helper definitions above to define FLAARLIB_API and FLAARLIB_LOCAL.
@@ -27,15 +27,15 @@
 // FLAARLIB_LOCAL is used for non-api symbols.
 
 #ifdef FLAARLIB_DLL // defined if FOX is compiled as a DLL
-  #ifdef FLAARLIB_DLL_EXPORTS // defined if we are building the FLAARLIB DLL (instead of using it)
-    #define FLAARLIB_API FLAARLIB_HELPER_DLL_EXPORT
-  #else
-    #define FLAARLIB_API FLAARLIB_HELPER_DLL_IMPORT
-  #endif // FLAARLIB_DLL_EXPORTS
-  #define FLAARLIB_LOCAL FLAARLIB_HELPER_DLL_LOCAL
+#ifdef FLAARLIB_DLL_EXPORTS // defined if we are building the FLAARLIB DLL (instead of using it)
+#define FLAARLIB_API FLAARLIB_HELPER_DLL_EXPORT
+#else
+#define FLAARLIB_API FLAARLIB_HELPER_DLL_IMPORT
+#endif // FLAARLIB_DLL_EXPORTS
+#define FLAARLIB_LOCAL FLAARLIB_HELPER_DLL_LOCAL
 #else // FLAARLIB_DLL is not defined: this means FLAARLIB is a static lib.
-  #define FLAARLIB_API
-  #define FLAARLIB_LOCAL
+#define FLAARLIB_API
+#define FLAARLIB_LOCAL
 #endif // FLAARLIB_DLL
 
 #ifndef FLAARLIB_H_
@@ -48,39 +48,60 @@
 
 #include "helper/UniqueVector.hpp"
 
-namespace flaarlib {
+namespace flaarlib
+{
 
 class FLModule;
-class InputModule;
+class FLInputModule;
+class FLRepository;
 
-class FLAARLIB_API Flaarlib {
+class FLAARLIB_API Flaarlib
+{
 public:
 	// singleton pattern
-	static Flaarlib * instance() {
+	static Flaarlib *instance()
+	{
 		static CGuard g;   // Speicherbereinigung
 		if (!_instance)
 			_instance = new Flaarlib();
 		return (_instance);
 	}
 
+public: // methods
+	bool init();
 	void addModule(FLModule *module);
-	void addInputModule(InputModule *inputModule);
+	void addInputModule(FLInputModule *inputModule);
 	void loopDetection();
 
-private:
-	static Flaarlib *_instance;
+public: // getter
+	FLRepository *flRepository()
+	{
+		return m_pRepository;
+	}
+
+
+private: // methods
 	Flaarlib();
 	Flaarlib(const Flaarlib &);
 	virtual ~Flaarlib();
 	void initializeLogging();
+	bool createRepository();
+
+private: // members
+	static Flaarlib *_instance;
 
 	bool m_configurationValid = false;
 	UniqueVector<std::string> m_modules;
-	std::map<std::string, InputModule*> m_inputModules;
-	class CGuard {
+	std::map<std::string, FLInputModule *> m_inputModules;
+	FLRepository *m_pRepository;
+
+	class CGuard
+	{
 	public:
-		~CGuard() {
-			if ( NULL != Flaarlib::_instance) {
+		~CGuard()
+		{
+			if ( NULL != Flaarlib::_instance)
+			{
 				delete Flaarlib::_instance;
 				Flaarlib::_instance = NULL;
 			}
